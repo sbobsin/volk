b32398a0cafb6c535589931c25f42c0e258ea51a
Scott Bobsin <scottbobsin@blueskychurch.com>
2024-01-16 14:53:54 -0800

    adding products model

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_16_225218) do
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
